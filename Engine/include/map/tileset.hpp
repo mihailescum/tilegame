0e0fbaa868a894808fe5fcde6d60a36cae2fccb6
Max Mihailescu <mihailescu.m@gmx.de>
2020-09-22 15:08:14 +0200

    Restructures CMakeLists

--- Engine/include/map/tileset.hpp
+++ Engine/include/map/tileset.hpp
@@ -1,10 +1,24 @@
 #ifndef __ENGINE_TILESET_H__
 #define __ENGINE_TILESET_H__
 
+#include <string>
+#include <vector>
+
+#include "tinyxml2.h"
+
+#include "tile.hpp"
+
 namespace engine
 {
     class Tileset
     {
+        private:
+            int firstGID = 0;
+            std::vector<Tile> tiles;
+        public:
+            Tileset() {}
+            ~Tileset() {}
+            void loadFromFile(const std::string filename);
     };
 } // namespace engine
 
