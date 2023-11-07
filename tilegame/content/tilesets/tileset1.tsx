c734f775590e18ba0a9ce20a4563ce9b87febd13
Max Mihailescu <1270849+mihailescum@users.noreply.github.com>
2023-11-07 09:32:50 +0100

    Switch to JSON Tiled format and included loader

--- tilegame/content/tilesets/tileset1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tileset1" tilewidth="32" tileheight="32" tilecount="15120" columns="120">
- <image source="../textures/tileset1_32x32.png" width="3840" height="4032"/>
-</tileset>
