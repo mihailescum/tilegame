f2c89db6ff9fc772e8074e6c13355078b9c8ec72
Max Mihailescu <1270849+mihailescum@users.noreply.github.com>
2023-11-13 14:35:08 +0100

    Added sol3 and linking to lua

--- tilegame/src/main.cpp
+++ tilegame/src/main.cpp
@@ -1,9 +1,22 @@
 #include "tilegame.hpp"
 
+#define SOL_ALL_SAFETIES_ON 1
+#include "sol/sol.hpp"
+
 int main(int argc, char *argv[])
+{
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    lua.script("print('bark bark bark!')");
+
+    return 0;
+}
+
+/*int main(int argc, char *argv[])
 {
     tilegame::Tilegame game(1200, 900);
     game.run();
 
     return 0;
-}
+}*/
